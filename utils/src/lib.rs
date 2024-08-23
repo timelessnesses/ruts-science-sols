f7b61ef92c0a913dbb4fe5f8ddaad9ca42cccd6a
Rukchad Wongprayoon <mooping3roblox@gmail.com>
2024-08-23 16:37:44 +0700

    hooray

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,7 +1,9 @@
 use std::io::{stdin, stdout, Write};
 pub fn input(question: Option<&str>) -> String {
-    print!("{}", question.unwrap_or(""));
-    stdout().flush().unwrap();
+    if let Some(q) = question {
+        print!("{q}");
+        stdout().flush().unwrap();
+    };
 
     let mut out = String::new();
     stdin().read_line(&mut out).unwrap();
