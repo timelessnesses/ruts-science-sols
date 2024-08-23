d1bc7accd75f768de14f1e631ae03d095b874036
Rukchad Wongprayoon <mooping3roblox@gmail.com>
2024-08-23 16:48:53 +0700

    Update lib.rs

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -7,7 +7,6 @@ pub fn input(question: Option<&str>) -> String {
 
     let mut out = String::new();
     stdin().read_line(&mut out).unwrap();
-    out.truncate(out.len() - 1); // \n
-    out.truncate(out.len() - 1); // \r
+    out.trim();
     out
 }
